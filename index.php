ed0b72a63e0fc9858aef9eff2bbe1497e5d7c48e
Marc <marc@marc.(none)>
2014-06-22 19:46:16 +0200

    First commit with web-schema

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include 'head.php';
+?>
+	<body>
+<?php
+include 'menu.php';
+?>		
+
+
+ 	<div id="map-canvas"></div>		
+
+	</body>
+
+
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+
+
+</html>
---
+++ menu.php
@@ -0,0 +1,27 @@
+<div class="container">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+					<nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+								<li class="gn-search-item">
+									<input placeholder="Search" type="search" class="gn-search">
+									<a class="gn-icon gn-icon-search"><span>Search</span></a>
+								</li>
+							
+								<li><a class="gn-icon gn-icon-cog">Settings</a></li>
+								<li><a class="gn-icon gn-icon-help">Help</a></li>
+							
+							</ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="earth.php">Earth</a></li>
+				<li><a>Moon</a></li>
+				<li><a>Mars</a></li>
+				
+				<li><a href="index.php"><span>LG</span></a></li>
+			</ul>
+
+		</div><!-- /container -->
