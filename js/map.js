152b6ec48cf7cb505e2602214e91ed0bc0983e87
Marc <lg@lg1.(none)>
2014-06-25 16:36:48 +0200

    Voice+Moon scripts added

---
+++ js/map.js
@@ -0,0 +1,79 @@
+/*
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+*/
+
+var map2;
+function initialize2() {
+  var mapOptions = {
+    zoom: 10,
+    center: new google.maps.LatLng(-34.397, 20.644)
+  };
+  map2 = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+
+var map;
+
+function ini_geo() {
+  var mapOptions = {
+    zoom: 10
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'You are here!'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+
+
+
+
+
+
+//google.maps.event.addDomListener(window, 'load', initialize);
+
